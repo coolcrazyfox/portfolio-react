0205eedf10b9bebb73d2f29a9b1c98765d10ea2b
coolcrazyfox <86570262+coolcrazyfox@users.noreply.github.com>
2022-02-07 21:34:43 +0300

    Updates

---
+++ precache-manifest.3e1a35228a6e6c80f481edf0448d4416.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec753b6c8646df04cf2d8a633807e530",
+    "url": "/portfplio-react/index.html"
+  },
+  {
+    "revision": "10502aa58f366cdd447c",
+    "url": "/portfplio-react/static/css/main.b5c9581b.chunk.css"
+  },
+  {
+    "revision": "373a2bad962f24451804",
+    "url": "/portfplio-react/static/js/2.ec00a2a8.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/portfplio-react/static/js/2.ec00a2a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "10502aa58f366cdd447c",
+    "url": "/portfplio-react/static/js/main.3096d534.chunk.js"
+  },
+  {
+    "revision": "acd4944c4db9a5ca55e1",
+    "url": "/portfplio-react/static/js/runtime-main.f69b0c02.js"
+  },
+  {
+    "revision": "b23c51398fbd84c2b9cd9dcb7ccdf665",
+    "url": "/portfplio-react/static/media/Social-network.b23c5139.jpg"
+  },
+  {
+    "revision": "4a79f8b3365e1359d82d1fd58f74d7b1",
+    "url": "/portfplio-react/static/media/todolist.4a79f8b3.jpg"
+  }
+]);
