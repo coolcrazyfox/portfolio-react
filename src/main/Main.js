07d61961bf5a9143d7aaea63057f087a04befe4d
coolcrazyfox <86570262+coolcrazyfox@users.noreply.github.com>
2022-02-22 23:20:33 +0300

    add parallax

--- src/main/Main.js
+++ src/main/Main.js
@@ -1,19 +1,24 @@
 import React from 'react';
 import s from './Main.module.css';
 import sCont from '../../src/common/styles/Container.module.css'
+import { Parallax } from 'react-parallax';
+import mainImage from './first1920x1080.jpg'
 
 export function Main() {
     return(
-    <div className={s.MainBlock}>
-        <div className={sCont.container}>
-            <div className={s.text}>
-                <span>Hey</span>
-                <h1>I am Andrei Malest</h1>
-                <p>Frontend Developer</p>
-            </div>
-            <div className={s.photo}></div>
-        </div>
+        <Parallax  bgImage={mainImage}  strength={500}>
+            <div className={s.MainBlock}>
+                <div className={sCont.container}>
+                    <div className={s.text}>
+                        <span>Hello</span>
+                        <h1>I am Andrei Malets</h1>
+                        <p>Frontend Developer</p>
+                    </div>
+                    <div className={s.photo}></div>
+                </div>
 
-    </div>
+            </div>
+        </Parallax>
+
     );
 }
