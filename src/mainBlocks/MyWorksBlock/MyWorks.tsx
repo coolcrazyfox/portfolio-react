237e4a2425fffd5a562c05fc2ec6db7418040c59
coolcrazyfox <86570262+coolcrazyfox@users.noreply.github.com>
2022-03-16 16:37:52 +0300

    create my works projects

--- src/mainBlocks/MyWorksBlock/MyWorks.tsx
+++ src/mainBlocks/MyWorksBlock/MyWorks.tsx
@@ -2,6 +2,8 @@ import s from './MyWorks.module.scss';
 import {WorksBox} from './WorksBox/WorksBox';
 import socialNet from './imgWorks/social.gif';
 import tList from './imgWorks/todolist.gif';
+import websitePr from './imgWorks/webSite.gif';
+import counterPr from './imgWorks/counter.gif';
 // @ts-ignore
 import Rotate from 'react-reveal/Fade'
 
@@ -24,6 +26,12 @@ export const MyWorks = () => {
     const todolistPrjImg = {
         backgroundImage: `url(${tList})`
     }
+    const webPrjImg = {
+        backgroundImage: `url(${websitePr})`
+    }
+    const counterPrjImg = {
+        backgroundImage: `url(${counterPr})`
+    }
     return (<div id={'MyWorksBlock'} className={blockTitle}>
         <Rotate top left>
             <div className={container}>
@@ -38,20 +46,16 @@ export const MyWorks = () => {
                             <span>k</span>
                             <span>s</span>
                         </h1>
-                        {/*<span>My</span> <p>works</p>*/}
                     </div>
                     <div className={MyWorksMain}>
                         <WorksBox href={'https://coolcrazyfox.github.io/social_network-v/#/'} styleImg={socialPrjImg} descriptions={'Look at my social network'}
                                   name={'social network'}/>
-                        {/*https://coolcrazyfox.github.io/counter-v2/*/}
-                        {/*https://coolcrazyfox.github.io/Social-network/*/}
-                        {/*https://coolcrazyfox.github.io/Todolist/*/}
                         <WorksBox href={'https://coolcrazyfox.github.io/my-counter-v/'} styleImg={todolistPrjImg} descriptions={'Look at my todolist'}
                                   name={'todolist'}/>
-                        <WorksBox href={'https://coolcrazyfox.github.io/social_network-v/#/'} styleImg={socialPrjImg} descriptions={'Look at my social network'}
-                                  name={'social network'}/>
-                        <WorksBox href={'https://coolcrazyfox.github.io/my-counter-v/'} styleImg={todolistPrjImg} descriptions={'Look at my todolist'}
-                                  name={'todolist'}/>
+                        <WorksBox href={'https://coolcrazyfox.github.io/web-site-v2/'} styleImg={webPrjImg} descriptions={'Look at my Website'}
+                                  name={'website'}/>
+                        <WorksBox href={'https://coolcrazyfox.github.io/my-counter-v/'} styleImg={counterPrjImg} descriptions={'Look at my counter'}
+                                  name={'counter'}/>
 
                     </div>
                 </div>
