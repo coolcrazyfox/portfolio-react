ae4f0b4213d437308d5f23f9c7c71fdaacc2d8bc
coolcrazyfox <86570262+coolcrazyfox@users.noreply.github.com>
2022-07-24 22:45:37 +0300

    change new my project my portfolio v2

--- src/mainBlocks/MyWorksBlock/MyWorks.tsx
+++ src/mainBlocks/MyWorksBlock/MyWorks.tsx
@@ -135,7 +135,7 @@ export const MyWorks = () => {
                                   name={'Rick & Morty'}/>
                         <WorksBox href={'https://coolcrazyfox.github.io/chess-game/'} styleImg={chessPrjImg}  descriptions={'Look at my chess-app'}
                                   name={'Chess-game-app'}/>
-                        <WorksBox href={''} styleImg={nextPrjImg}  descriptions={'My next project'}
+                        <WorksBox href={'https://coolcrazyfox.github.io/portfolio-v2/'} styleImg={nextPrjImg}  descriptions={'My next project'}
                                   name={'my next project'}/>
                     </div>
                 </div>
