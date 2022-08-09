9bf7c696caf8a9fb38e92c0341ba6a8b6f4156ca
coolcrazyfox <86570262+coolcrazyfox@users.noreply.github.com>
2022-08-09 20:34:59 +0300

    change App

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
 import {MouseContextProvider} from "./context/MouseContext";
 
 
@@ -16,4 +16,4 @@ ReactDOM.render(
 );
 
 
-reportWebVitals();
+
